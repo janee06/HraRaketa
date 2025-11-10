da1eef9f26b5f9b325d466835c0097c01256430a
janee06 <s2022BockovaJana@skolabaltaci.cz>
2025-11-10 17:14:40 +0100

    Add project files.

---
+++ HraRaketa/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace HraRaketa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtRaketa = new System.Windows.Forms.Label();
+            this.pozadi = new System.Windows.Forms.GroupBox();
+            this.meteorit = new System.Windows.Forms.PictureBox();
+            this.pictureBox_raketa = new System.Windows.Forms.PictureBox();
+            this.txtscore = new System.Windows.Forms.Label();
+            this.txtzivoty = new System.Windows.Forms.Label();
+            this.txtpalivo = new System.Windows.Forms.Label();
+            this.Boxskore = new System.Windows.Forms.TextBox();
+            this.Boxpalivo = new System.Windows.Forms.TextBox();
+            this.pictureBoxzivoty1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxzivoty2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxzivoty3 = new System.Windows.Forms.PictureBox();
+            this.pozadi.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.meteorit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_raketa)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxzivoty1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxzivoty2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxzivoty3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtRaketa
+            // 
+            this.txtRaketa.AutoSize = true;
+            this.txtRaketa.Location = new System.Drawing.Point(396, 13);
+            this.txtRaketa.Name = "txtRaketa";
+            this.txtRaketa.Size = new System.Drawing.Size(98, 18);
+            this.txtRaketa.TabIndex = 0;
+            this.txtRaketa.Text = "Hra Raketa\r\n";
+            // 
+            // pozadi
+            // 
+            this.pozadi.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pozadi.Controls.Add(this.meteorit);
+            this.pozadi.Controls.Add(this.pictureBox_raketa);
+            this.pozadi.Location = new System.Drawing.Point(12, 63);
+            this.pozadi.Name = "pozadi";
+            this.pozadi.Size = new System.Drawing.Size(837, 383);
+            this.pozadi.TabIndex = 1;
+            this.pozadi.TabStop = false;
+            // 
+            // meteorit
+            // 
+            this.meteorit.Image = ((System.Drawing.Image)(resources.GetObject("meteorit.Image")));
+            this.meteorit.Location = new System.Drawing.Point(154, 97);
+            this.meteorit.Name = "meteorit";
+            this.meteorit.Size = new System.Drawing.Size(80, 75);
+            this.meteorit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.meteorit.TabIndex = 1;
+            this.meteorit.TabStop = false;
+            // 
+            // pictureBox_raketa
+            // 
+            this.pictureBox_raketa.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox_raketa.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_raketa.Image")));
+            this.pictureBox_raketa.Location = new System.Drawing.Point(409, 299);
+            this.pictureBox_raketa.Name = "pictureBox_raketa";
+            this.pictureBox_raketa.Size = new System.Drawing.Size(60, 58);
+            this.pictureBox_raketa.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_raketa.TabIndex = 0;
+            this.pictureBox_raketa.TabStop = false;
+            // 
+            // txtscore
+            // 
+            this.txtscore.AutoSize = true;
+            this.txtscore.Location = new System.Drawing.Point(12, 474);
+            this.txtscore.Name = "txtscore";
+            this.txtscore.Size = new System.Drawing.Size(60, 18);
+            this.txtscore.TabIndex = 3;
+            this.txtscore.Text = "Skóre:";
+            this.txtscore.Click += new System.EventHandler(this.txtscore_Click);
+            // 
+            // txtzivoty
+            // 
+            this.txtzivoty.AutoSize = true;
+            this.txtzivoty.Location = new System.Drawing.Point(368, 474);
+            this.txtzivoty.Name = "txtzivoty";
+            this.txtzivoty.Size = new System.Drawing.Size(60, 18);
+            this.txtzivoty.TabIndex = 4;
+            this.txtzivoty.Text = "Životy:";
+            // 
+            // txtpalivo
+            // 
+            this.txtpalivo.AutoSize = true;
+            this.txtpalivo.Location = new System.Drawing.Point(691, 474);
+            this.txtpalivo.Name = "txtpalivo";
+            this.txtpalivo.Size = new System.Drawing.Size(61, 18);
+            this.txtpalivo.TabIndex = 5;
+            this.txtpalivo.Text = "Palivo:";
+            // 
+            // Boxskore
+            // 
+            this.Boxskore.Location = new System.Drawing.Point(77, 471);
+            this.Boxskore.Name = "Boxskore";
+            this.Boxskore.Size = new System.Drawing.Size(100, 26);
+            this.Boxskore.TabIndex = 0;
+            // 
+            // Boxpalivo
+            // 
+            this.Boxpalivo.Location = new System.Drawing.Point(758, 471);
+            this.Boxpalivo.Name = "Boxpalivo";
+            this.Boxpalivo.Size = new System.Drawing.Size(100, 26);
+            this.Boxpalivo.TabIndex = 7;
+            // 
+            // pictureBoxzivoty1
+            // 
+            this.pictureBoxzivoty1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxzivoty1.Image")));
+            this.pictureBoxzivoty1.Location = new System.Drawing.Point(434, 464);
+            this.pictureBoxzivoty1.Name = "pictureBoxzivoty1";
+            this.pictureBoxzivoty1.Size = new System.Drawing.Size(47, 39);
+            this.pictureBoxzivoty1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxzivoty1.TabIndex = 8;
+            this.pictureBoxzivoty1.TabStop = false;
+            // 
+            // pictureBoxzivoty2
+            // 
+            this.pictureBoxzivoty2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxzivoty2.Image")));
+            this.pictureBoxzivoty2.Location = new System.Drawing.Point(487, 464);
+            this.pictureBoxzivoty2.Name = "pictureBoxzivoty2";
+            this.pictureBoxzivoty2.Size = new System.Drawing.Size(47, 39);
+            this.pictureBoxzivoty2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxzivoty2.TabIndex = 9;
+            this.pictureBoxzivoty2.TabStop = false;
+            // 
+            // pictureBoxzivoty3
+            // 
+            this.pictureBoxzivoty3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxzivoty3.Image")));
+            this.pictureBoxzivoty3.Location = new System.Drawing.Point(540, 464);
+            this.pictureBoxzivoty3.Name = "pictureBoxzivoty3";
+            this.pictureBoxzivoty3.Size = new System.Drawing.Size(47, 39);
+            this.pictureBoxzivoty3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxzivoty3.TabIndex = 10;
+            this.pictureBoxzivoty3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 515);
+            this.Controls.Add(this.pictureBoxzivoty3);
+            this.Controls.Add(this.pictureBoxzivoty2);
+            this.Controls.Add(this.pictureBoxzivoty1);
+            this.Controls.Add(this.Boxpalivo);
+            this.Controls.Add(this.Boxskore);
+            this.Controls.Add(this.txtpalivo);
+            this.Controls.Add(this.txtzivoty);
+            this.Controls.Add(this.txtscore);
+            this.Controls.Add(this.pozadi);
+            this.Controls.Add(this.txtRaketa);
+            this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Hra Raketa";
+            this.pozadi.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.meteorit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_raketa)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxzivoty1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxzivoty2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxzivoty3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtRaketa;
+        private System.Windows.Forms.GroupBox pozadi;
+        private System.Windows.Forms.Label txtscore;
+        private System.Windows.Forms.Label txtzivoty;
+        private System.Windows.Forms.Label txtpalivo;
+        private System.Windows.Forms.TextBox Boxskore;
+        private System.Windows.Forms.PictureBox pictureBox_raketa;
+        private System.Windows.Forms.TextBox Boxpalivo;
+        private System.Windows.Forms.PictureBox meteorit;
+        private System.Windows.Forms.PictureBox pictureBoxzivoty1;
+        private System.Windows.Forms.PictureBox pictureBoxzivoty2;
+        private System.Windows.Forms.PictureBox pictureBoxzivoty3;
+    }
+}
+
---
+++ HraRaketa/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace HraRaketa
+{
+    public partial class Form1 : Form
+    {
+        int raketaRychlost = 10;
+        int meteorrychlost = 10;
+        int skore = 0;
+        int zivoty = 3;
+        int palivo = 100;
+        Random rnd = new Random();
+
+        bool pohybVlevo = false;
+        bool pohybVpravo = false;
+
+        PictureBox[] meteority;
+        PictureBox[] srdce;
+        Timer t = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            this.KeyDown += Okno_KeyDown;
+            this.KeyUp += Okno_KeyUp;
+
+            pictureBox_raketa.Left = pozadi.Width / 2 - pictureBox_raketa.Width / 2;
+            pictureBox_raketa.Top = pozadi.Height - pictureBox_raketa.Height - 10;
+
+           
+            PictureBox meteorit2 = new PictureBox();
+            meteorit2.Size = meteorit.Size;
+            meteorit2.SizeMode = PictureBoxSizeMode.Zoom;
+            meteorit2.Image = meteorit.Image;
+            pozadi.Controls.Add(meteorit2);
+
+            meteority = new PictureBox[] { meteorit, meteorit2 };
+
+            foreach (var m in meteority)
+                ResetMeteorit(m);
+
+            
+            srdce = new PictureBox[] { pictureBoxzivoty1, pictureBoxzivoty2, pictureBoxzivoty3 };
+
+           
+            t.Interval = 50;
+            t.Tick += GameLoop;
+            t.Start();
+        }
+
+        private void Okno_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) pohybVlevo = true;
+            if (e.KeyCode == Keys.Right) pohybVpravo = true;
+        }
+
+        private void Okno_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) pohybVlevo = false;
+            if (e.KeyCode == Keys.Right) pohybVpravo = false;
+        }
+
+        private void GameLoop(object sender, EventArgs e)
+        {
+            
+            if (pohybVlevo && pictureBox_raketa.Left > 0 && palivo > 0)
+            {
+                pictureBox_raketa.Left -= raketaRychlost;
+                palivo--;
+            }
+            if (pohybVpravo && pictureBox_raketa.Right < pozadi.Width && palivo > 0)
+            {
+                pictureBox_raketa.Left += raketaRychlost;
+                palivo--;
+            }
+
+            palivo = Math.Max(palivo, 0);
+
+            for (int i = 0; i < meteority.Length; i++)
+                MoveMeteorit(meteority[i]);
+
+           
+            for (int i = 0; i < srdce.Length; i++)
+                srdce[i].Visible = (i < zivoty);
+
+           
+            Boxskore.Text = "Skóre: " + skore;
+            Boxpalivo.Text = "Palivo: " + palivo + "%";
+        }
+
+        private void MoveMeteorit(PictureBox meteorit)
+        {
+            meteorit.Top += meteorrychlost;
+
+            
+            if (meteorit.Bounds.IntersectsWith(pictureBox_raketa.Bounds))
+            {
+                zivoty--;
+                palivo = 100;
+                ResetMeteorit(meteorit);
+
+                if (zivoty <= 0)
+                {
+                    t.Stop();
+                    MessageBox.Show("Konec hry!\nSkóre: " + skore);
+                    this.Close();
+                }
+            }
+
+            if (meteorit.Top > pozadi.Height)
+            {
+                ResetMeteorit(meteorit);
+                skore++;
+            }
+        }
+
+        private void ResetMeteorit(PictureBox meteorit)
+        {
+            meteorit.Top = -meteorit.Height;
+            meteorit.Left = rnd.Next(0, pozadi.Width - meteorit.Width);
+        }
+
+        private void txtscore_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
